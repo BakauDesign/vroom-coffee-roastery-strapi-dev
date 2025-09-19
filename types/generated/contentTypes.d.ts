e2c8f0aac658f280c09a5e71557fcee6861b3dbc
BakauDesign <hi.bakaudesign@gmail.com>
2025-09-19 11:22:40 +0700

    feat: remove whatsapp_number field

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -433,7 +433,6 @@ export interface ApiPesananProdukRoastedBeanPesananProdukRoastedBean
       Schema.Attribute.Required;
     nama_pembeli: Schema.Attribute.String & Schema.Attribute.Required;
     nomor_resi: Schema.Attribute.Text;
-    nomor_whatsapp: Schema.Attribute.BigInteger & Schema.Attribute.Required;
     produk_yang_dibeli: Schema.Attribute.Component<
       'komponen-produk.produk-yang-dibeli',
       true
@@ -486,7 +485,6 @@ export interface ApiPesananProdukToolPesananProdukTool
     nama_layanan_pengiriman: Schema.Attribute.String &
       Schema.Attribute.Required;
     nama_pembeli: Schema.Attribute.String & Schema.Attribute.Required;
-    nomor_whatsapp: Schema.Attribute.BigInteger & Schema.Attribute.Required;
     produk_yang_dibeli: Schema.Attribute.Component<
       'komponen-produk.produk-yang-dibeli',
       true
